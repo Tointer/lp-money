3e4fbdf307655f4c476ded95142008c998141935
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-15 12:25:12 +0300

    fix tests

--- contracts/test/LPMoney.t.sol
+++ contracts/test/LPMoney.t.sol
@@ -9,18 +9,34 @@ import {IGhoToken} from '../src/interfaces/IGhoToken.sol';
 import {GhoMock} from "./mocks/GhoMock.sol";
 import {LpCollectionMock} from "./mocks/LpCollectionMock.sol";
 import {PriceOracleMock} from "./mocks/PriceOracleMock.sol";
+import {ACLManagerMock} from "./mocks/ACLManagerMock.sol";
+import {PoolAddressProviderMock} from "./mocks/PoolAddressProviderMock.sol";
 
 contract LPMoneyTest is Test {
     IGhoToken public ghoToken;
     LpCollectionMock public lpCollection;
     PriceOracleMock public priceOracle;
+    ACLManagerMock public aclManager;
+    PoolAddressProviderMock public poolAddressProvider;
     LPMoney public lpMoney;
+    
 
     function setUp() public {
         ghoToken = new GhoMock();
         lpCollection = new LpCollectionMock(); 
         priceOracle = new PriceOracleMock();
-        lpMoney = new LPMoney(address(0), address(ghoToken), INonfungiblePositionManager(lpCollection), priceOracle);
+        aclManager = new ACLManagerMock();
+        poolAddressProvider = new PoolAddressProviderMock();
+
+        poolAddressProvider.setACLManger(address(aclManager));
+
+        lpMoney = new LPMoney(
+            address(0), 
+            ghoToken, 
+            INonfungiblePositionManager(lpCollection), 
+            priceOracle, 
+            address(poolAddressProvider)
+        );
     }
 
     function test_mint() public {
