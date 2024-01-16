076113b51729d6122b7fff32c4163427f391dee6
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-16 14:39:08 +0300

    change frontend from next.js to simple react+vite

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ConnectKitButton } from 'connectkit';
+import React from 'react';
+
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <ConnectKitButton />
+    </div>
+  );
+}
+
+export default App;
