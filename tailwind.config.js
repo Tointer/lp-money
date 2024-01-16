47df5f7633606eec20a86317bfbf992cb9283ed4
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-16 20:38:16 +0300

    add wiggle animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': {},
+          '25%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(2deg)' },
+          '75%': { transform: 'rotate(-1deg)' },
+        },
+      }
+    },
   },
   plugins: [],
 }
