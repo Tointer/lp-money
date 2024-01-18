5555f7975ca14453f7b3509d0284291947396f6d
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-18 21:46:24 +0300

    update price oracle mock

--- contracts/test/mocks/PriceOracleMock.sol
+++ contracts/test/mocks/PriceOracleMock.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 import {ILPpriceOracle} from '../../src/interfaces/ILPpriceOracle.sol';
 import {IUniswapV3Pool} from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import {AggregatorV2V3Interface} from "bunni-oracle/external/AggregatorV2V3Interface.sol";
 
 contract PriceOracleMock is ILPpriceOracle{
     uint256 public currentMockPrice;
@@ -12,7 +13,9 @@ contract PriceOracleMock is ILPpriceOracle{
         int24 tickUpper,
         uint128 liquidity,
         uint32 uniV3OracleSecondsAgo,
-        uint256 chainlinkPriceMaxAgeSecs
+        uint256 chainlinkPriceMaxAgeSecs,
+        AggregatorV2V3Interface feed0,
+        AggregatorV2V3Interface feed1
     ) external override view returns (uint256 valueUSD){
         return currentMockPrice;
     }
