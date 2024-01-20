62b177a3c57d904479fe4b4492729811491037f3
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-20 15:10:30 +0300

    remove old code from top page

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ import img from  "./ghoaave.webp"
 import { sepolia, useAccount } from 'wagmi';
 
 function App() {
-  const [selectedPosition, setSelectedPosition] = 
-  useState<{id: number, name: string, mintAmount: number}>({id: 0, name: 'Select position ', mintAmount: 0});
-
   const account = useAccount();
   
   return (
@@ -23,25 +20,15 @@ function App() {
       </div>
 
       <div className='flex flex-col w-full bg-neutral-900 h-40 items-center'>
-        <div className='text-s text-opacity-30 text-neutral-100 select-none text-center p-2'>
-          {selectedPosition.name}
-        </div>
-        <button className='text-2xl text-wrap text-neutral-100 text-center p-2 w-fit rounded-[16px] bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500'>
-          Mint {selectedPosition.mintAmount}
-        </button>
       </div>
 
       <div className='w-full m-2 '>
-        {account.address !== undefined && <PositionsView onPositionSelected={onPositionSelected} userAccount={account.address}/>}
+        {account.address !== undefined && <PositionsView userAccount={account.address}/>}
       </div>
 
 
     </div>
   );
-
-  function onPositionSelected(id: number, name: string, mintAmount: number) {
-    setSelectedPosition({id, name, mintAmount});
-  }
 
 
 }
