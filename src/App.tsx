9592a9fb7a5689bae899f7a4fce9757c6cd4636e
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-21 01:44:13 +0300

    add logo and decorative text

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { ConnectKitButton } from 'connectkit';
 import PositionsView from './components/PositionsView';
 import {useState, useEffect} from 'react';
 import img from  "./ghoaave.webp"
+import logo from "./logo.png"
 import { sepolia, useAccount } from 'wagmi';
 import {useErc20BalanceOf} from './generated'
-import { useContractRead } from 'wagmi'
 import { formatDecimals } from './utils/contracts';
 
 function App() {
@@ -13,7 +13,10 @@ function App() {
   
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="flex justify-center p-2 w-full bg-gradient-to-r from-green-400 to-blue-500">
+      <div className="flex justify-center p-2 w-full bg-gradient-to-r from-green-400 to-blue-500 relative">
+        <div className='h-12 absolute left-2 top-1'>
+          <img src={logo} className='h-12'/>
+        </div>
         <ConnectKitButton />
         <div className='bg-neutral-800 text-neutral-100 rounded-[16px] text-center flex items-center pr-2 ml-2'>
           <img src={img} className='rounded-full size-10'/>
@@ -23,17 +26,22 @@ function App() {
         </div>
       </div>
 
-      <div className='flex flex-col w-full bg-neutral-900 h-24 items-center'>
+      <div className='flex flex-col w-full bg-neutral-900 text-gray-500 text-2xl h-24 justify-center text-nowrap text-clip overflow-hidden select-none'>
+      LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY LP MONEY
       </div>
 
       <div className='w-full m-2 '>
-        {account.address !== undefined && <PositionsView userAccount={account.address}/>}
+        {account.address !== undefined && <PositionsView userAccount={account.address} onGHOBalanceChange={refetchBalance}/>}
       </div>
 
 
     </div>
   );
 
+  function refetchBalance() {
+    console.log("refetching balance");
+    GHOBalance.refetch();
+  }
 
 }
 
