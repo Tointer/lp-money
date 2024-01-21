b8f057cc7bf28ed413ae8ee1188b2ca6cedb66c9
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-21 14:33:27 +0300

    remove unused chains from config, make arbitrum first

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,7 @@ import { getDefaultConfig } from 'connectkit';
 export const config = createConfig(
     getDefaultConfig({
       appName: 'ConnectKit Vite demo',
-      //infuraId: import.meta.env.VITE_INFURA_ID,
-      //alchemyId:  import.meta.env.VITE_ALCHEMY_ID,
-      chains: [mainnet, polygon, optimism, arbitrum],
+      chains: [arbitrum, mainnet],
       walletConnectProjectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID!,
     })
 );
